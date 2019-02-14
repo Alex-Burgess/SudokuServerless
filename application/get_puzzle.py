8568a57833dd3569abec55c2cb956485bde4f193
Alex-Burgess <9518982+Alex-Burgess@users.noreply.github.com>
2019-02-14 12:18:51 +0000

    Fixing bug with random number generation and adding better logging

--- application/get_puzzle.py
+++ application/get_puzzle.py
@@ -20,12 +20,13 @@ def get_puzzle_from_s3():
 
     # TODO - get maximum number of puzzles in bucket
     # TODO - s3 role should least permissions, right now it has all permissons
-    random_key = str(random.randint(0, 7)) + '.json'
+    random_key = str(random.randint(1, 8)) + '.json'
     print ('DEBUG: Puzzle Key: ' + str(random_key))
     
-    print("Getting S3 object...")
+    print("DEBUG: Getting S3 object (" + random_key + ") from bucket (" + bucket_name +")")
     obj = s3.Object(bucket_name, random_key)
     puzzle_data = obj.get()['Body'].read().decode('utf-8')
+    print ("DEBUG: Puzzle object: " + puzzle_data)
     
     return puzzle_data
 
