a2485153729a3ce7f94c97768c517dc6e0fa57b7
Alex-Burgess <9518982+Alex-Burgess@users.noreply.github.com>
2019-06-21 17:25:58 +0100

    Added col elimination function to solve module

--- services/solvePuzzle/solve_puzzle/solve.py
+++ services/solvePuzzle/solve_puzzle/solve.py
@@ -1,31 +1,40 @@
 import json
-from solve_puzzle import validate
 from solve_puzzle import common
 
 
 def solve_puzzle(puzzle):
     for loop in range(0, 5):
+        # Method 1 - Cell Elimination
+        for r in range(0, 9):
+            for c in range(0, 9):
+                puzzle = cell_elimination(puzzle, r, c)
+
         # Method 2 - Row elimination
         for r in range(0, 9):
             puzzle = row_elimination(puzzle, r)
 
-        # Method 1 - Cell Elimination
-        for r in range(0, 9):
-            for c in range(0, 9):
-                cell_contains_value = common.cell_contains_number(puzzle, r, c)
-                if not cell_contains_value:
-                    result = eliminate_cell_values(puzzle, r, c)
+        # Method 3 - Col elimination
+        for c in range(0, 9):
+            puzzle = col_elimination(puzzle, c)
 
-                    if result['status']:
-                        puzzle = update_cell(puzzle, r, c, result['values'][0])
-
-        print("INFO: Loop (" + str(loop) + "), puzzle update: " + json.dumps(puzzle))
+        print("INFO: Finished Loop (" + str(loop) + "), puzzle update: " + json.dumps(puzzle))
 
         if puzzle_complete(puzzle):
             return {'puzzle': puzzle, 'status': True}
 
     # Puzzle not solved
     return {'puzzle': puzzle, 'status': False}
+
+
+def cell_elimination(puzzle, row_num, col_num):
+    cell_contains_value = common.cell_contains_number(puzzle, row_num, col_num)
+    if not cell_contains_value:
+        result = eliminate_cell_values(puzzle, row_num, col_num)
+
+        if result['status']:
+            puzzle = update_cell(puzzle, row_num, col_num, result['values'][0])
+
+    return puzzle
 
 
 def row_elimination(puzzle, row_num):
@@ -52,7 +61,7 @@ def row_elimination(puzzle, row_num):
             col = common.get_column(puzzle, cell)
             if test_val in col:
                 unsolved_cell_list.remove(cell)
-                # print("DEBUG: Removed cell ({},{}) of Col ({}) contains val ({}).".format(row_num, cell, col, test_val))
+                # print("DEBUG: Removed cell ({},{}) as Col ({}) contains val ({}).".format(row_num, cell, col, test_val))
                 continue
 
             # Test if a cell can be eliminated, due to grid match.
@@ -60,7 +69,7 @@ def row_elimination(puzzle, row_num):
             grid = common.get_grid(puzzle, grid_num)
             if test_val in grid:
                 unsolved_cell_list.remove(cell)
-                # print("DEBUG: Removed cell ({},{}) of Grid ({}) contains val ({}).".format(row_num, cell, grid, test_val))
+                # print("DEBUG: Removed cell ({},{}) as Grid ({}) contains val ({}).".format(row_num, cell, grid, test_val))
                 continue
 
         if len(unsolved_cell_list) == 1:
@@ -69,6 +78,51 @@ def row_elimination(puzzle, row_num):
         # else:
         #     print("DEBUG: All cells in row {} ({}) could not be eliminated for value ({}). Remaining cell references ({})".format(
         #         row_num, row, test_val, unsolved_cell_list))
+
+    return puzzle
+
+
+def col_elimination(puzzle, col_num):
+    col = common.get_column(puzzle, col_num)
+
+    # Determine remaining values to solve in list.
+    value_test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    remaining_values = elimate_list_values(value_test_list, col)
+    # print("DEBUG: {} ({}) has remaining values ({})".format(type, col, remaining_values))
+
+    for test_val in remaining_values:
+        # print("DEBUG: Checking if all but one of the cells in Col ({}) can be elimindated for value ({}).".format(col_num, test_val))
+
+        # Tracking list, when this reaches a size of 1, for the test value, the cell is solved.
+        unsolved_cell_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+        for cell in range(0, 9):
+            # Test if cell can be eliminated, as already contains value
+            if col[cell] > 0:
+                unsolved_cell_list.remove(cell)
+                continue
+
+            # Test if cell can be eliminated, due to a row match.
+            row = common.get_row(puzzle, cell)
+            if test_val in row:
+                unsolved_cell_list.remove(cell)
+                # print("DEBUG: Removed cell ({},{}) as Row ({}) contains val ({}).".format(cell, col_num, row, test_val))
+                continue
+
+            # Test if a cell can be eliminated, due to grid match.
+            grid_num = common.get_grid_number(puzzle, cell, col_num)
+            grid = common.get_grid(puzzle, grid_num)
+            if test_val in grid:
+                unsolved_cell_list.remove(cell)
+                # print("DEBUG: Removed cell ({},{}) as Grid ({}) contains val ({}).".format(cell, col_num, grid, test_val))
+                continue
+
+        if len(unsolved_cell_list) == 1:
+            print("INFO: Eliminated all cells in Col {} ({}) for value ({}) to reference ({})".format(col_num, col, test_val, unsolved_cell_list))
+            puzzle = update_cell(puzzle, unsolved_cell_list[0], col_num, test_val)
+        # else:
+        #     print("DEBUG: All cells in Col {} ({}) could not be eliminated for value ({}). Remaining cell references ({})".format(
+        #         col_num, col, test_val, unsolved_cell_list))
 
     return puzzle
 
@@ -87,7 +141,6 @@ def puzzle_complete(puzzle):
 def row_col_grid_complete(puzzle, type):
     test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     for i in range(0, 9):
-        # TODO
         type_list = []
         if type == "rows":
             type_list = common.get_row(puzzle, i)
